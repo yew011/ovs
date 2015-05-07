a32bb1b6ecd21ec0ad5d61f181fd90db137c23b3
Alex Wang <alexw@nicira.com>
2015-05-07 11:00:34 -0700

    Add ovn-controller-gw.
    Step 3. Register the gateway_ports entry in Chassis Table.

--- ovn/controller-gw/gateway.c
+++ ovn/controller-gw/gateway.c
@@ -29,17 +29,23 @@ VLOG_DEFINE_THIS_MODULE(gateway);
 
 #define ENCAP_TYPE "vxlan"
 
-/* Registers the VTEP gateway in ovn-sb. */
+/* Registers the VTEP gateway in ovn-sb.
+ * xxx Does not support changing any of these mid-run. */
 static void
 register_gw(struct controller_gw_ctx *ctx)
 {
     const struct sbrec_chassis *chassis_rec;
-    const struct vteprec_physical_switch *cfg;
-    const char *encap_ip;
+    const struct vteprec_physical_switch *pswitch;
     struct sbrec_encap *encap_rec;
-    static bool inited = false;
-    int retval = TXN_TRY_AGAIN;
     struct ovsdb_idl_txn *txn;
+    const char *encap_ip;
+    int retval = TXN_TRY_AGAIN;
+    static bool inited = false;
+    int i;
+
+    if (inited) {
+        return;
+    }
 
     SBREC_CHASSIS_FOR_EACH(chassis_rec, ctx->ovnsb_idl) {
         if (!strcmp(chassis_rec->name, ctx->chassis_id)) {
@@ -47,39 +53,25 @@ register_gw(struct controller_gw_ctx *ctx)
             break;
         }
     }
+    ovs_assert(!chassis_rec);
 
     /* xxx Need to support more than one encap ip. */
-    cfg = vteprec_physical_switch_first(ctx->vtep_idl);
-    if (!cfg) {
-        VLOG_INFO("No Open_vSwitch row defined");
+    pswitch = vteprec_physical_switch_first(ctx->vtep_idl);
+    if (!pswitch) {
+        VLOG_INFO("No Physical_vSwitch row defined.");
         return;
     }
 
     /* TODO: Allow multiple tunnel_ips. */
-    if (!cfg->n_tunnel_ips) {
+    if (!pswitch->n_tunnel_ips) {
         VLOG_INFO("Could not find tunnel ip");
         return;
     }
-    encap_ip = cfg->tunnel_ips[0];
+    encap_ip = pswitch->tunnel_ips[0];
 
-    if (chassis_rec) {
-        int i;
-
-        for (i = 0; i < chassis_rec->n_encaps; i++) {
-            if (!strcmp(chassis_rec->encaps[i]->type, ENCAP_TYPE)
-                && !strcmp(chassis_rec->encaps[i]->ip, encap_ip)) {
-                /* Nothing changed. */
-                inited = true;
-                return;
-            } else if (!inited) {
-                VLOG_WARN("Chassis config changing on startup, make sure "
-                          "multiple chassis are not configured : %s/%s->%s/%s",
-                          chassis_rec->encaps[i]->type,
-                          chassis_rec->encaps[i]->ip,
-                          ENCAP_TYPE, encap_ip);
-            }
-
-        }
+    if (!pswitch->n_ports) {
+        VLOG_INFO("Could not find any physical ports");
+        return;
     }
 
     txn = ovsdb_idl_txn_create(ctx->ovnsb_idl);
@@ -87,17 +79,27 @@ register_gw(struct controller_gw_ctx *ctx)
                               "ovn-controller-gw: registering gateway chassis '%s'",
                               ctx->chassis_id);
 
-    if (!chassis_rec) {
-        chassis_rec = sbrec_chassis_insert(txn);
-        sbrec_chassis_set_name(chassis_rec, ctx->chassis_id);
-    }
+    chassis_rec = sbrec_chassis_insert(txn);
 
+    sbrec_chassis_set_name(chassis_rec, ctx->chassis_id);
     encap_rec = sbrec_encap_insert(txn);
     sbrec_encap_set_type(encap_rec, ENCAP_TYPE);
     sbrec_encap_set_ip(encap_rec, encap_ip);
     sbrec_chassis_set_encaps(chassis_rec, &encap_rec, 1);
 
-    /* TODO: update the gateway_ports column and the Gateway table. */
+    const char **pp_names = xmalloc(sizeof *pp_names * pswitch->n_ports);
+    struct sbrec_gateway **gws = xmalloc(sizeof *gws * pswitch->n_ports);
+
+    for (i = 0; i < pswitch->n_ports; i++) {
+        pp_names[i] = pswitch->ports[i]->name;
+        gws[i] = sbrec_gateway_insert(txn);
+        sbrec_gateway_set_attached_port(gws[i], pp_names[i]);
+    }
+    sbrec_chassis_set_gateway_ports(chassis_rec, pp_names, gws,
+                                    pswitch->n_ports);
+    free(pp_names);
+    free(gws);
+
     retval = ovsdb_idl_txn_commit_block(txn);
     if (retval != TXN_SUCCESS && retval != TXN_UNCHANGED) {
         VLOG_INFO("Problem registering chassis: %s",
