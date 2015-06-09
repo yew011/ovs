f697a82600ca5eccd8f9544235b219f6fd464ad7
Alex Wang <alexw@nicira.com>
2015-06-09 10:13:34 -0700

    ovn-sbctl: Add ovn-sbctl.
    This commit adds ovn-sbctl to ovn family by using the db-ctl-base
    library.
    
    As a side effect, we move the ovn-nbctl/ovn-sbctl related files
    into ovn/utilities.
    
    Signed-off-by: Alex Wang <alexw@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>
    
    ---
    V2->V3:
    - rebase to base.
    - adopt Russell's review comments.
    - move ovn-nbctl/sbctl related files into ovn/utilities.
    
    PATCH->V2:
    - change command add/del-ch to add/del-chassis.
    - refine the manual based on comments from Ben.

--- ovn/utilities/automake.mk
+++ ovn/utilities/automake.mk
@@ -10,3 +10,21 @@ EXTRA_DIST += \
 
 DISTCLEANFILES += \
     ovn/utilities/ovn-ctl.8
+
+# ovn-nbctl
+bin_PROGRAMS += ovn/utilities/ovn-nbctl
+ovn_utilities_ovn_nbctl_SOURCES = ovn/utilities/ovn-nbctl.c
+ovn_utilities_ovn_nbctl_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenvswitch.la
+
+man_MANS += ovn/utilities/ovn-nbctl.8
+EXTRA_DIST += ovn/utilities/ovn-nbctl.8.xml
+DISTCLEANFILES += ovn/utilities/ovn-nbctl.8
+
+# ovn-sbctl
+bin_PROGRAMS += ovn/utilities/ovn-sbctl
+ovn_utilities_ovn_sbctl_SOURCES = ovn/utilities/ovn-sbctl.c
+ovn_utilities_ovn_sbctl_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenvswitch.la
+
+MAN_ROOTS += ovn/utilities/ovn-sbctl.8.in
+man_MANS += ovn/utilities/ovn-sbctl.8
+DISTCLEANFILES += ovn/utilities/ovn-sbctl.8
